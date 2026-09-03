e0cbc002c33a7a186740d0ed34c9d7eaf5c686ec
BernardoBosel <bernardobosel03@gmail.com>
2026-09-03 20:28:56 -0300

    Add files via upload

---
+++ Ponteiro/Exercicios pra aprender/Ex1.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+
+void inverter(int *v, int n) { 
+    int *inicio = v; 
+    int *fim = v + n - 1; 
+    int temporario; 
+
+    while (inicio < fim) { 
+        temporario = *inicio; 
+        *inicio = *fim; 
+        *fim = temporario; 
+        inicio++; 
+        fim--; 
+    } 
+} 
+
+int main(void) { 
+    int v[] = {10, 20, 30, 40, 50}; 
+    int n = 5; 
+
+    inverter(v, n); 
+
+    for (int *p = v; p < v + n; p++) { 
+        printf("%d ", *p); 
+    } 
+
+    printf("\n"); 
+    return 0;
+}
